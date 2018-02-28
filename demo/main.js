3ce5b791122c225bc856194a546b7e770d1e232c
李想 <lixiang@lixiangdeMacBook-Pro.local>
2018-02-28 15:11:06 +0800

    基本工作环境搭建完成

---
+++ demo/main.js
@@ -0,0 +1,30 @@
+$(function () {
+     
+    // 图片滚动
+
+    // 模式1:leftLoop
+    // jeSlide({
+    //     mainCell: "#findslider",
+    //     navCell: ".hd ul",
+    //     conCell: ".bd ul",
+    //     effect: "leftLoop",
+    //     duration: 4,
+    //     sLoad: "data-pic",
+    //     isTouch: true,
+    //     showNav: true,//自动分页
+    //     autoPlay: $('#findslider .bd li').length > 1 ? true : false  //自动播放
+    // });
+    // 模式2:curtain 
+    jeSlide({
+        mainCell: "#findslider",
+        navCell: ".hd ul",
+        conCell: ".bd ul",
+        effect: "curtain",
+        duration: 4,
+//      pageStateCell:".pageState",
+        sLoad: "data-pic",
+        isTouch:true,
+        showNav: true,//自动分页
+        autoPlay: true  //自动播放
+    });
+})
